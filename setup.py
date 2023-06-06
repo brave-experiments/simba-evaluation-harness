65a8db2fd91a09030851dbac43f8a8793d15c242
Stefanos Laskaridis <stefanos@brave.com>
2023-06-06 17:29:45 +0300

    Missing einops dependency

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
     python_requires=">=3.9",
     install_requires=[
         "datasets>=2.0.0",
+        "einops>=0.6.1",
         "jsonlines",
         "numexpr",
         "openai>=0.6.4",
